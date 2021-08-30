401cc3cab11ab6064c5fb33b8a0401911f357f10
MatheusProg <matheus-dias1@outlook.com>
2021-08-30 20:11:29 -0300

    removendo o OWl Carousel

--- js/app.js
+++ js/app.js
@@ -5,22 +5,3 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
-
-
-$('.owl-carousel').owlCarousel({
-    stagePadding: 60,
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:6
-        }
-    }
-})
